cf2262555a4194c118de5e1d8cd8f4a07fda40bd
Andrej Mihajlov <and@mullvad.net>
2020-11-19 14:09:59 +0100

    Remove unused import

--- WireGuardKit/Sources/WireGuardKit/WireGuardKit.swift
+++ WireGuardKit/Sources/WireGuardKit/WireGuardKit.swift
@@ -2,7 +2,6 @@
 // Copyright © 2018-2019 WireGuard LLC. All Rights Reserved.
 
 import Foundation
-import libwg_go
 
 public func getWireGuardVersion() -> String {
     return WireGuardAdapter.version
